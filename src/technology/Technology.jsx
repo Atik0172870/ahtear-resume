b652ec78af01f5b84a2302526503c610aa0b7ca1
Atik hasan <atik0172870@hotmail.com>
2025-02-13 17:45:28 +0600

    change and update to make responsive

--- src/technology/Technology.jsx
+++ src/technology/Technology.jsx
@@ -6,10 +6,10 @@ function Home() {
 
     return (
         <div className='card technology rounded-0 border-0 mt-5'>
-            <div className='main-card card-body d-flex flex-lg-row flex-column gap-4'>
-                <div class="glass-card shadow-lg card w-25 w-100">
-                    <h3 className='text-info m-0'>Back-End Technology</h3>
-                    <hr />
+            <div className='main-card card-body'>
+                <div class="glass-card shadow-lg">
+                    <h3 className='text-warning m-0'>Back-End Technology</h3>
+                    <hr className='text-warning' />
                     <ul>
                         <li>C#.NET, ASP.NET Core, .NET Core</li>
                         <li>Microservices Ocelot, JWT Token, </li>
@@ -18,36 +18,36 @@ function Home() {
 
                     </ul>
                 </div>
-                <div class="glass-card shadow-lg card w-25 w-100">
-                    <h3 className='text-info m-0'>Client-Side Technology</h3>
-                    <hr />
+                <div class="glass-card shadow-lg">
+                    <h3 className='text-warning m-0'>Client-Side Technology</h3>
+                    <hr className='text-warning' />
                     <ul>
                         <li>ReactJs, Angular, Typescript,JavaScript</li>
                         <li>AngularJS, jQuery, AJAX</li>
                         <li>Bootstrap 5, HTML5, CSS3,SASS</li>
                     </ul>
                 </div>
-                <div class="glass-card shadow-lg card w-25 w-100" >
-                    <h3 className='text-info m-0'>Data Base</h3>
-                    <hr />
+                <div class="glass-card shadow-lg" >
+                    <h3 className='text-warning m-0'>Data Base</h3>
+                    <hr className='text-warning' />
                     <ul>
                         <li>MS SQL Server, ORACLE 11g, MY SQL,SQLite</li>
                         <li>PostgreSQL, MongoDB (Document-based) </li>
                         <li>DynamoDB (AWS-managed NoSQL)</li>
                     </ul>
                 </div>
-                <div class="glass-card shadow-lg card w-25 w-100">
-                    <h3 className='text-info m-0'>DevOps Things</h3>
-                    <hr />
+                <div class="glass-card shadow-lg">
+                    <h3 className='text-warning m-0'>DevOps Things</h3>
+                    <hr className='text-warning' />
                     <ul>
                         <li>Azure, Docker, Kubernetes, Minikube, MicroK8s</li>
                         <li></li>
                     </ul>
                 </div>
 
-                <div class="glass-card shadow-lg card w-25 w-100" >
-                    <h3 className='text-info m-0'>Source Control</h3>
-                    <hr />
+                <div class="glass-card shadow-lg" >
+                    <h3 className='text-warning m-0'>Source Control</h3>
+                    <hr className='text-warning' />
                     <ul>
                         <li>Azure DevOps, GitHub, Bitbucket, SVN</li>
                         <li>TFS, SourceTree</li>

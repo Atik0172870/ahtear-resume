a1beec72ff6741bd65cfbc4944c09e7dd46f4d1b
atik@0172870@gmail.com <atik@0172870@gmail.com>
2025-06-03 18:23:32 +0600

    add base url in router like BrowserRouter basename='/ahtear-resume'

--- src/main.jsx
+++ src/main.jsx
@@ -6,7 +6,7 @@ import Navbar from './navbar/Navbar.jsx'
 
 createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename='/ahtear-resume'>
         <Navbar />
         <App />
     </BrowserRouter>

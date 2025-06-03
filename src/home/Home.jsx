22347b8eb85664fc6b2191e2d685fa016767ec90
atik@0172870@gmail.com <atik@0172870@gmail.com>
2025-06-03 19:17:25 +0600

    fixed home page issue and add Python, FastAPI

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -18,9 +18,10 @@ function Home() {
                 <div className='border border-0 border-bottom w-100 mb-2 border-warning'></div>
                 {/* <p className="text-info m-0 fw-bold p-font-size-tech">{'[ Napco Security Technologies - USA ]'}</p> */}
                 <span>
-                    <p className="text-white p-font-size m-0">{'C# .NET, ASP.NET MVC,.NET Core,Microservice, RabbitMQ ,'}</p>
-                    <p className="text-white p-font-size m-0">{' Ocelot, JWT Token | ReactJs, Angular,Typescript ,JavaScript '}</p>
-                    <p className="text-white p-font-size m-0">{'| Azure DevOps, Docker, Kubernetes, Minikube, Microk8s, Kafka'}</p>
+                    <p className="text-white p-font-size m-0">{'C# .NET, .NET Core, ASP.NET MVC, Microservice,'}</p>
+                    <p className="text-white p-font-size m-0">{'RabbitMQ, Ocelot, JWT Token | ReactJs, Angular,'}</p>
+                    <p className="text-white p-font-size m-0">{'Typescript, JavaScript| Azure DevOps, Docker,'}</p>
+                    <p className="text-white p-font-size m-0">{'Kubernetes, Minikube, Microk8s, Kafka | Python, FastAPI'}</p>
                 </span>
                 <div className='mt-auto d-flex gap-4'>
                     <a href='https://www.facebook.com/share/1HciuJSaVV/' target='_blank' title='Facebook'>    <img src={facebook} height={25} /></a>

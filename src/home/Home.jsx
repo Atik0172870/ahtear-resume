e250520f3486b2eedcec85c410490fa51b4ee8ad
atik@0172870@gmail.com <atik@0172870@gmail.com>
2025-06-20 21:11:16 +0600

    change contact midify section

--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -14,26 +14,27 @@ function Home() {
             <div className="card-body text-white glass-card border-0 rounded-0 shadow-lg">
                 <h4 className="card-title">MD AHTEAR RAHMAN</h4>
                 <img className="card-img-bottom rounded-circle profile_pic border-1 border border-warning" src={Ahtear_rahman} alt="Card image" />
-                <p className="text-warning m-0 fw-bold p-font-size-degination mt-3">{'Senior Full-Stack Software Engineer'}</p>
-                <p className="text-info p-font-size-tech m-0 mb-2">{'[ C#.NET, .NET CORE | React | Angular ]'}</p>
-                <div className='border border-0 border-bottom w-100 mb-2 border-warning'></div>
-                {/* <p className="text-info m-0 fw-bold p-font-size-tech">{'[ Napco Security Technologies - USA ]'}</p> */}
-                <span>
-                    <p className="text-white p-font-size m-0">{'C# .NET, .NET Core, ASP.NET MVC, Microservice,'}</p>
-                    <p className="text-white p-font-size m-0">{'RabbitMQ, Ocelot, JWT Token | ReactJs, Angular,'}</p>
-                    <p className="text-white p-font-size m-0">{'Typescript, JavaScript| Azure DevOps, Docker,'}</p>
-                    <p className="text-white p-font-size m-0">{'Kubernetes, Minikube, Microk8s, Kafka | Python, FastAPI'}</p>
-                </span>
-                <div className='mt-auto  mb-0 d-flex flex-column'>
-                    <span className='text-info fw-bold p-font-size-tech d-flex align-items-center justify-content-center mb-1'>Contact (Dhaka, Bangladesh.)</span>
-                    <div className='border border-0 border-bottom w-100 mb-2 border-warning'></div>
-                    <span className='m-0 p-font-size'>Cell : +8801728706930, +8801516162081</span>
-                    <span className='m-0 p-font-size'>Email : Atik0172870@gmail.com, Atik017287069@gmail.com</span>
-                    <span className='m-0 p-font-size'>LinkedIn : https://www.linkedin.com/in/md-ahtear-rahman-b8043a15a/</span>
-                    <span className='m-0 p-font-size'>GitHub : https://github.com/Atik0172870</span>
-                    <span className='m-0 p-font-size mt-3'></span>
 
+                <div className='d-flex flex-column'>
+                    <span className="text-warning m-0 fw-bold p-font-size-degination mt-3">{'Senior Full-Stack Software Engineer'}</span>
+                    <span className="text-info fw-bold p-font-size-tech d-flex align-items-center justify-content-center mb-1">{'[ C#.NET, .NET CORE | React | Angular ]'}</span>
+                    {/* <p className="text-info m-0 fw-bold p-font-size-tech">{'[ Napco Security Technologies - USA ]'}</p> */}
+                    <div className='border border-0 border-bottom w-100 mb-2 border-warning'></div>
+                    <span className="p-font-size">{'C# .NET, .NET Core, ASP.NET MVC, Microservice,'}</span>
+                    <span className="p-font-size">{'RabbitMQ, Ocelot, JWT Token | ReactJs, Angular,'}</span>
+                    <span className="p-font-size">{'Typescript, JavaScript| Azure DevOps, Docker,'}</span>
+                    <span className="p-font-size">{'Kubernetes, Minikube, Microk8s, Kafka | Python, FastAPI'}</span>
                 </div>
+                    <div className='mt-auto d-flex flex-column'>
+                        <span className='text-info fw-bold p-font-size-tech d-flex align-items-center justify-content-center mb-1'>Contact (Dhaka, Bangladesh.)</span>
+                        <div className='border border-0 border-bottom w-100 mb-2 border-warning'></div>
+                        <span className='p-font-size'>{'Cell : +8801728706930, +8801516162081'}</span>
+                        <span className='p-font-size'>{'Email : Atik0172870@gmail.com, Atik017287069@gmail.com'}</span>
+                        <span className='p-font-size'>{'LinkedIn : https://www.linkedin.com/in/md-ahtear-rahman-b8043a15a/'}</span>
+                        <span className='p-font-size'>{'GitHub : https://github.com/Atik0172870'}</span>
+                        <span className='p-font-size'>{'CV_Link : https://atik0172870.github.io/ahtear-resume'}</span>
+                    </div>
+
                 <div className='mt-auto d-flex gap-4'>
                     <a href='https://www.facebook.com/share/1HciuJSaVV/' target='_blank' title='Facebook'>    <img src={facebook} height={25} /></a>
                     <a href='https://www.linkedin.com/in/md-ahtear-rahman-b8043a15a/' target='_blank' title='LinkedIn'>    <img src={linkedin} height={25} /></a>
